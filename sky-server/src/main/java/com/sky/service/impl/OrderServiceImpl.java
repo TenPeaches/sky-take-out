0293ac2d797426f928fb06a1399d3233b04536c1
TenPeaches <2060853750@qq.com>
2025-05-07 09:27:23 +0800

    来单提醒业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sky.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
@@ -18,6 +19,7 @@ import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
 import com.sky.vo.OrderVO;
+import com.sky.websocket.WebSocketServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,7 +29,9 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -46,6 +50,8 @@ public class OrderServiceImpl implements Orderservice {
     @Autowired
     private UserMapper userMapper;
     private Orders orders;
+    @Autowired
+    private WebSocketServer webSocketServer;
 
 
     /**
@@ -141,6 +147,15 @@ public class OrderServiceImpl implements Orderservice {
         Integer OrderStatus = Orders.TO_BE_CONFIRMED;  //订单状态，待接单
         LocalDateTime check_out_time = LocalDateTime.now();//更新支付时间
         orderMapper.updateStatus(OrderStatus, OrderPaidStatus, check_out_time, this.orders.getId());
+
+        //通过websocket向客户端浏览器发送消息 type orderId content
+        Map map = new HashMap();
+        map.put("type", 1);//1代表来单提醒 2代表客户催单
+        map.put("orderId", this.orders.getId());
+        map.put("content", "订单号："+this.orders.getNumber());
+        String json = JSON.toJSONString(map);
+        webSocketServer.sendToAllClient(json);
+
         return vo;
     }
 
@@ -163,6 +178,7 @@ public class OrderServiceImpl implements Orderservice {
                 .build();
 
         orderMapper.update(orders);
+
     }
 
     /**
